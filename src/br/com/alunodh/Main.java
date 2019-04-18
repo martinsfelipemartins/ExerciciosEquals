f99ef85fa470f4289455524c6660890e43bff526
Digital <lkmlmk>
2019-04-17 22:19:14 -0300

    Initial commit

---
+++ src/br/com/alunodh/Main.java
@@ -0,0 +1,14 @@
+package br.com.alunodh;
+
+public class Main {
+
+    public static void main(String[] args) {
+    Pessoa felipe = new Pessoa("Felipe", 123);
+    Pessoa fernando = new Pessoa("Fernando", 123);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        
+    }
+}
---
+++ src/br/com/alunodh/Pessoa.java
@@ -0,0 +1,13 @@
+package br.com.alunodh;
+
+public class Pessoa {
+
+    private String nome;
+    private Integer RG;
+
+    public Pessoa(String nome, Integer RG) {
+        this.nome = nome;
+        this.RG = RG;
+    }
+
+}
